6fd06166806621f986024dcec3792ce5a3240b8b
kelf <kivenf3776@gmail.com>
2025-08-18 21:23:08 +0800

    add conform.nvim as format tool

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+  'stevearc/conform.nvim',
+  opts = {},
+  config = function()
+    conform = require("conform")
+    conform.setup({
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+    })
+  end,
+}
