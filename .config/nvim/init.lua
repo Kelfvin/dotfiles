00e5c04db9645339c5bd88ae9bbc524593e31966
kelf <kivenf3776@gmail.com>
2026-04-02 13:32:48 +0800

    迁移到neovim 0.12

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,11 @@
+-- basic settings
 require("config.options")
 require("config.keymaps")
-require("config.lazy")
 
+-- lsp settings
 vim.lsp.enable 'lua_ls'
 vim.lsp.enable 'pyright_ls'
 vim.lsp.enable 'ruff_ls'
 
-vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+--plugins
+require("plugins.catppuccin")
