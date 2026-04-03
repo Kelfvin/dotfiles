c0b837f825b80eb3610c704bb132388a647f940b
kelf <kivenf3776@gmail.com>
2026-04-03 11:03:59 +0800

    add lua formatter

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,12 +1,15 @@
 vim.pack.add({
-	{ src = "https://github.com/stevearc/conform.nvim" },
+  { src = "https://github.com/stevearc/conform.nvim" },
 })
 
 conform = require("conform")
 conform.setup({
-	format_on_save = {
-		-- These options will be passed to conform.format()
-		timeout_ms = 500,
-		lsp_format = "fallback",
-	},
+  formatters_by_ft = {
+    lua = { "stylua" },
+  },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
 })
