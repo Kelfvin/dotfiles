aa3a713926f48e21a1da80efdb2065a1939d5eec
kelf <kelfvin@163.com>
2026-04-11 10:13:41 +0800

    add more alias and condition load

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,11 +2,6 @@
 # │                          Alias                           │
 # ╰──────────────────────────────────────────────────────────╯
 alias python="python3"
-
-alias ls="eza"
-alias ll="eza -l"
-
-alias c="clear"
 alias pip="pip3"
 
 # tmux alias
@@ -18,5 +13,37 @@ alias lg="lazygit"
 
 alias zsh_reload="source ~/.zshrc"
 
-# replace cd with zoxide
-alias cd="z"
+## Modified commands
+alias ping='ping -c 5'
+
+
+if command -v eza>/dev/null 2>&1; then
+  alias ls="eza"
+  alias ll="eza -l"
+else
+  alias ll="ls -l"
+fi
+
+if command -v dust>/dev/null 2>&1; then
+  alias du="dust"
+else
+  alias du="du -h -c"
+fi
+
+if command -v duf>/dev/null 2>&1; then
+  alias df="duf"
+else
+  alias df="df -h"
+fi
+
+
+alias grep='grep --color=auto'
+alias mkdir='mkdir -p -v'
+
+## Safety features
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -I'                    # 'rm -i' prompts for every file
+
+alias ff="fastfetch"
+alias c="clear"
