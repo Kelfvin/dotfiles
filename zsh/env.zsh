1a605f14fb43496ce5d217a807020772920a6d11
kelf <kelfvin@163.com>
2026-04-28 22:01:51 +0800

    update

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -37,6 +37,9 @@ export GOOGLE_CLOUD_PROJECT=charged-sled-465304-e0
 
 export MUSICFOX_ROOT=$HOME/.config/go-musicfox
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# fnm
+FNM_PATH="/home/kelf/.local/share/fnm"
+if [ -d "$FNM_PATH" ]; then
+  export PATH="$FNM_PATH:$PATH"
+  eval "$(fnm env --shell zsh)"
+fi
